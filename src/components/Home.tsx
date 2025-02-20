2d8363159dd0d3291a209c2e4b1c0bcf06653a62
Shafin Ahmed <kazishafin@gmail.com>
2025-02-20 03:04:44 -0500

    Add down arrow to Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,15 @@
 import "./Home.css";
 import Underline from "../assets/underline.svg";
+import Down from "../assets/down.svg";
 
 export const Home = () => {
   return (
     <div className="home">
       <p>Shafin Ahmed</p>
-      <img src={Underline} alt="Underline" />
+      <img className="underline" src={Underline} alt="Underline" />
+      <div className="down-arrow-container">
+        <img className="down-arrow" src={Down} alt="Down Arrow" />
+      </div>
     </div>
   );
 };
