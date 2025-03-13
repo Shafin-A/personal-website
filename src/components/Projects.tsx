5e4f23428141975e6316830611008391f1e6f136
Shafin Ahmed <kazishafin@gmail.com>
2025-03-13 18:34:13 -0400

    Add ValStats projects and style changes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,6 +2,8 @@ import "./Projects.css";
 import { Details } from "./Details";
 import DiscordStorage from "../assets/DiscordStorage.png";
 import { GithubLogo } from "@phosphor-icons/react";
+import ValStats from "../assets/ValStats.png";
+import { Link } from "@phosphor-icons/react";
 
 export const Projects = () => {
   return (
@@ -9,28 +11,68 @@ export const Projects = () => {
       <h1>PROJECTS</h1>
       <div className="projects-container">
         <Details summary="DiscordStrorage">
-          <p>
-            DiscordStorage is a web application that allows users to manage
-            files and folders using a Discord server as the backend storage. It
-            provides a user-friendly interface to upload, download, and manage
-            files and folders.
-          </p>
-          <a
-            href="https://github.com/Shafin-A/DiscordStorage"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={DiscordStorage} alt="DiscordStorage" />
-          </a>
-          <a
-            href="https://github.com/Shafin-A/DiscordStorage"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <GithubLogo size={32} color="rgb(3, 54, 5)" />
-          </a>
+          <div className="details-content">
+            <div className="details-text">
+              <p>
+                DiscordStorage is a web application that allows users to manage
+                files and folders using a Discord server as the backend storage.
+                It provides a user-friendly interface to upload, download, and
+                manage files and folders.
+              </p>
+            </div>
+            <div className="details-image">
+              <img src={DiscordStorage} alt="Image for DiscordStorage" />
+            </div>
+            <div className="details-links">
+              <a
+                href="https://github.com/Shafin-A/DiscordStorage"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <GithubLogo size={32} color="rgb(3, 54, 5)" />
+              </a>
+            </div>
+          </div>
         </Details>
-        <Details summary="Project 2">Lorem ipsum</Details>
+        <Details summary="ValStats">
+          <div className="details-content">
+            <div className="details-text">
+              <p>
+                A fullstack website for VALORANT player statistics inspired by
+                websites like Blitz.gg and Tracker.gg. The website uses
+                HenrikDev's unnoffical API to fetch match data and display it in
+                a user-friendly interface. It also allows users to sign up and
+                comment directly on player profiles!
+              </p>
+            </div>
+            <div className="details-image">
+              <img src={ValStats} alt="Image for ValStats" />
+            </div>
+            <div className="details-links">
+              <a
+                href="https://github.com/Shafin-A/val-stats"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <GithubLogo size={32} color="rgb(3, 54, 5)" />
+              </a>
+              <a
+                href="https://github.com/Shafin-A/val-stats-server"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <GithubLogo size={32} color="rgb(3, 54, 5)" />
+              </a>
+              <a
+                href="https://val-stats.fly.dev/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Link size={32} color="rgb(3, 54, 5)" />
+              </a>
+            </div>
+          </div>
+        </Details>
       </div>
     </div>
   );
